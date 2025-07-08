bfda9c6d7f2f30fcdd11090b14602e704484c085
eden <noedenkim@gmail.com>
2025-07-08 21:24:36 +0900

    feat: complete Level 1 UI
    with dummy data

--- PokemonContactApp/Modules/ContactList/Controller/ContactListViewController.swift
+++ PokemonContactApp/Modules/ContactList/Controller/ContactListViewController.swift
@@ -7,15 +7,77 @@
 
 import UIKit
 import SnapKit
-import Alamofire
 
 class ContactListViewController: UIViewController {
+    
+    private let friendListLabel: UILabel = {
+        let label = UILabel()
+        label.text = "친구 목록"
+        label.font = .systemFont(ofSize: 20, weight: .bold)
+        return label
+    }()
+    
+    private let addButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("추가", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 17, weight: .medium)
+        button.titleLabel?.textColor = .white
+        return button
+    }()
+    
+    private lazy var contactListTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ContactListCell.self, forCellReuseIdentifier: ContactListCell.id)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureUI()
     }
 
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        [
+            friendListLabel,
+            addButton,
+            contactListTableView
+        ].forEach { view.addSubview($0) }
+        
+        friendListLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(80)
+            $0.centerX.equalToSuperview()
+        }
+        
+        addButton.snp.makeConstraints {
+            $0.centerY.equalTo(friendListLabel)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+        contactListTableView.snp.makeConstraints {
+            $0.top.equalTo(friendListLabel.snp.bottom).offset(40)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
 
+extension ContactListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        80
+    }
+}
+
+extension ContactListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ContactListCell.id) as? ContactListCell else { return UITableViewCell() }
+        cell.configureCell()
+        return cell
+    }
 }
 
--- PokemonContactApp/Modules/ContactList/View/ContactListCell.swift
+++ PokemonContactApp/Modules/ContactList/View/ContactListCell.swift
@@ -5,3 +5,66 @@
 //  Created by 김이든 on 7/8/25.
 //
 
+import UIKit
+
+final class ContactListCell: UITableViewCell {
+    
+    static let id = "ContactListCell"
+    
+    private let contactImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 25
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.systemGray.cgColor
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private let numberLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        [   contactImageView,
+            nameLabel,
+            numberLabel
+        ].forEach { contentView.addSubview($0)}
+        
+        contactImageView.snp.makeConstraints {
+            $0.size.equalTo(50)
+            $0.leading.equalToSuperview().inset(40)
+            $0.centerY.equalToSuperview()
+        }
+        
+        nameLabel.snp.makeConstraints {
+            $0.leading.equalTo(contactImageView.snp.trailing).offset(20)
+            $0.centerY.equalToSuperview()
+        }
+        
+        numberLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(40)
+            $0.centerY.equalToSuperview()
+        }
+    }
+    
+    public func configureCell() {
+        nameLabel.text = "김이든"
+        numberLabel.text = "010-0000-0000"
+    }
+}
